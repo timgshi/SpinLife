669f24291a327b12a54fc767dc79481415cbdb6e
Tim Shi <timshi@ideo.com>
2017-04-09 21:13:03 +0900

    Initial Commit

---
+++ SpinLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpinLife
+//
+//  Created by Tim Shi on 2017/04/09.
+//  Copyright © 2017 Tim Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
